98de9c1bb68c20afe81da5c9859cb30f205e03d2
tanjavetter04 <tanja-vetter@gmx.net>
2024-05-02 17:21:40 +0200

    .

--- Java/_05_filter/Example.java
+++ Java/_05_filter/Example.java
@@ -1,12 +1,11 @@
 package _05_filter;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Example {
     public static void main(String[] args) {
-        List<Integer> temperatures_celsius = Arrays.asList(25, 30, 22, 18, 28, 35, 20);
+        List<Integer> temperatures_celsius = List.of(25, 30, 22, 18, 28, 35, 20);
 
         List<Integer> high_temperatures = temperatures_celsius.stream()
                                             .filter(temp -> temp > 25)
